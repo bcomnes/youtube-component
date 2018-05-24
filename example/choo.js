c044cf37367264014a8526bddf4041f2aebc6888
Bret Comnes <bcomnes@gmail.com>
2018-05-24 10:20:18 -0700

    Simplify build

--- example/choo.js
+++ example/choo.js
@@ -1,37 +1,31 @@
 var html = require('nanohtml')
 var choo = require('choo')
+var NCCache = require('nanocomponent-cache')
 var YoutubeComponent = require('../')
 
-var video1 = new YoutubeComponent({attr: {
-  width: 480,
-  height: 270
-}})
-var video2 = new YoutubeComponent({attr: {
-  width: 480,
-  height: 270
-}})
-var video3 = new YoutubeComponent({attr: {
-  width: 480,
-  height: 270
-}})
-var video4 = new YoutubeComponent({attr: {
-  width: 480,
-  height: 270
-}})
+var c = new NCCache()
+
+c.register(YoutubeComponent, {
+  args: [
+    {
+      attr: { width: 480, height: 270 }
+    }
+  ]
+})
 
 var app = choo()
 app.route('/', mainView)
-if (typeof window !== 'undefined') app.mount('body')
+if (typeof window !== 'undefined') app.mount('#app')
 
 function mainView (state, emit) {
   return html`
-    <body>
       <div id="app">
-        <h1>Embed some vids</h1>
-        ${video1.render('https://www.youtube.com/watch?v=jnGtXnpZYDY&t=910s')}
-        ${video2.render('https://www.youtube.com/watch?v=iK6t9mJCykw')}
-        ${video3.render('https://vimeo.com/200671029')}
-        ${video4.render('http://www.dailymotion.com/video/x2ipgus')}
-      </div>
-    </body>`
+        <main class="markdown-body">
+          <h1><a href="https://github.com/bcomnes/youtube-component">Embed some vids</a></h1>
+          ${c.get('video1').render('https://www.youtube.com/watch?v=jnGtXnpZYDY&t=910s')}
+          ${c.get('video2').render('https://www.youtube.com/watch?v=bAojxWZRVKk')}
+          ${c.get('video3').render('https://vimeo.com/200671029')}
+          ${c.get('video4').render('http://www.dailymotion.com/video/x2ipgus')}
+        </main>
+      </div>`
 }
